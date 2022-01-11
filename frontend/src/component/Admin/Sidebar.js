fe399d2848e77cec5a3afb3f1f5476a07c97fe3e
Sangam choudhary <Sangamchoudhary7@gmail.com>
2022-01-11 21:56:14 +0530

    modified logo

--- frontend/src/component/Admin/Sidebar.js
+++ frontend/src/component/Admin/Sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./sidebar.css";
-import logo from "../../images/logo.png";
+import logo22 from "../../images/logo22.png";
 import { Link } from "react-router-dom";
 import { TreeView, TreeItem } from "@material-ui/lab";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
@@ -16,7 +16,7 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <Link to="/">
-        <img src={logo} alt="Ecommerce" />
+        <img src={logo22} alt="Ecommerce" />
       </Link>
       <Link to="/admin/dashboard">
         <p>
